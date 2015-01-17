f1c5ba9dc442f03f19881c4ddf4c57d9564d3da5
k-masa <wyrbtt@gmail.com>
2015-01-17 15:02:14 +0900

    Initial Commit

---
+++ UnixTime_Mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  UnixTime_Mac
+//
+//  Created by Kato Masaya on 2015/01/17.
+//  Copyright (c) 2015年 Kato Masaya. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ UnixTime_Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UnixTime_Mac
+//
+//  Created by Kato Masaya on 2015/01/17.
+//  Copyright (c) 2015年 Kato Masaya. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
