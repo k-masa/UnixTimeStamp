cbd4ac24e4d35154cb2372bd50d72fadf04e20c3
k-masa <wyrbtt@gmail.com>
2015-01-17 16:53:04 +0900

    ひとまずできた

--- UnixTime_Mac/ViewController.swift
+++ UnixTime_Mac/ViewController.swift
@@ -14,23 +14,70 @@ class ViewController: NSViewController {
     @IBOutlet weak var unixTextField: NSTextField!
     @IBOutlet weak var resultTextField: NSTextField!
 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let date = NSDate()
+        self.datePicker.dateValue = date
+        self.resultTextField.stringValue = self.getUnitTime(self.datePicker.dateValue)
     }
 
     override var representedObject: AnyObject? {
         didSet {
-        // Update the view, if already loaded.
+
         }
     }
     
+    //====================================================================================================
+    // MARK: IBAction Methods -
+    
+    /*====================================================================================================
+    *
+    */
     @IBAction func pushClipboardBtn(sender: AnyObject) {
+        var pastBoard = NSPasteboard.generalPasteboard()
+        var str: AnyObject? = pastBoard.stringForType(NSPasteboardTypeString)
         
+        if let pastObj: AnyObject = str {
+            let pastStr = pastObj as? String
+            if let pStr = pastStr {
+                let time = atof(pStr)
+                if time == 0 {
+                    return
+                }
+                self.unixTextField.stringValue = "\(pStr)"
+                self.resultTextField.stringValue = self.getDateFromUnixTime(time)
+            }
+        }
     }
-
-
+    @IBAction func valueChangeDatePicker(sender: NSDatePicker) {
+        self.resultTextField.stringValue = self.getUnitTime(sender.dateValue)
+        self.setPastBoard(self.resultTextField.stringValue)
+    }
+    
+    @IBAction func valueChangedUnixTextField(sender: NSTextField) {
+        let unix = atof(sender.stringValue)
+        if unix == 0 {
+            return
+        }
+        self.resultTextField.stringValue = self.getDateFromUnixTime(unix)
+        self.setPastBoard(self.resultTextField.stringValue)
+    }
+    /*======================================================
+    *   Helper Methods
+    */
+    func getDateFromUnixTime(time: NSTimeInterval) -> String {
+        var date = NSDate(timeIntervalSince1970: time);
+        return NSString.stringFromDate(date, formatString: "YYYY/MM/dd HH:mm:ss")
+    }
+    func getUnitTime(date: NSDate) -> String {
+        var time = date.timeIntervalSince1970
+        return NSString(format: "%0.0f", time)
+    }
+    func setPastBoard(string: String) -> Bool {
+        var pastboard = NSPasteboard.generalPasteboard()
+        pastboard.clearContents()
+        let seccess = pastboard.setString(string, forType: NSPasteboardTypeString)
+        return seccess
+    }
 }
-
